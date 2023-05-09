ab1f9f7324cb43e9cf125b150dd1acd40a7fcb34
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-05-09 20:07:57 -0300

    Game seed

--- src/main/java/com/api/dslist/entities/Game.java
+++ src/main/java/com/api/dslist/entities/Game.java
@@ -16,7 +16,9 @@ public class Game {
     private String platforms;
     private String imgUrl;
     private Double score;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
